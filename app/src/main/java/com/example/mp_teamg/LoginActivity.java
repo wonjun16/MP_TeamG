ba515b95ae92b992bf6e7af61d7ad3c2e7273d91
kkm0120 <121284199+kkm0120@users.noreply.github.com>
2023-05-13 20:19:42 +0900

    add splach page, and set this as first activity

--- app/src/main/java/com/example/mp_teamg/LoginActivity.java
+++ app/src/main/java/com/example/mp_teamg/LoginActivity.java
@@ -1,7 +1,80 @@
 package com.example.mp_teamg;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-    //로그인 관련 함수와 인텐트 만들어둘 것
+import androidx.appcompat.app.AppCompatActivity;
 
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mIdEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginButton;
+    private TextView mErrorTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mIdEditText = findViewById(R.id.edit_text_id);
+        mPasswordEditText = findViewById(R.id.edit_text_pw);
+        mLoginButton = findViewById(R.id.button_login);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mIdEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+/*
+                if (username.isEmpty() || password.isEmpty()) {
+                } else if (!isValidCredentials(username, password)) {
+                    // mErrorTextView.setVisibility(View.VISIBLE);
+                    // mErrorTextView.setText(R.string.error_invalid_credentials);
+                } else {
+                    // 로그인 처리
+                }
+ */
+            }
+        });
+
+
+        // 패스워드 보이기/숨기기 버튼 등록
+        ImageView passwordToggle = findViewById(R.id.ic_visibility);
+
+        // 패스워드 토글 아이콘 클릭 시 패스워드 보이기/숨기기
+        passwordToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPasswordEditText.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                    // 패스워드가 보이는 상태일 때
+                    mPasswordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordToggle.setImageResource(R.drawable.ic_visibility);
+                } else {
+                    // 패스워드가 숨겨진 상태일 때
+                    mPasswordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordToggle.setImageResource(R.drawable.ic_visibility);
+                }
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+    //private boolean isValidCredentials(String username, String password) {
+        // 유효한 로그인 정보인지 검사
+        //return username.equals("username") && password.equals("password");
+    //}
 }
--- app/src/main/java/com/example/mp_teamg/SplashActivity.java
+++ app/src/main/java/com/example/mp_teamg/SplashActivity.java
@@ -3,21 +3,29 @@ package com.example.mp_teamg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_TIME = 1000; // 2초 동안 스플래시 화면 유지
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        // 스플래시 화면이 보이는 동안의 작업을 수행
+        // 스플래시 화면이 끝나면 다음 액티비티로 이동. -> 나중에 로그인 액티비티로 이동
 
-        // 스플래시 화면이 끝나면 다음 액티비티로 이동. -> 나중에 로그인 액티비티로 이동하도록 할 것
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
+
     }
 }
